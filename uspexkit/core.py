d0876355b33e1450418aa7d2ecc85cd7e988e2a0
FengGo <gfeng.alan@foxmail.com>
2026-07-02 23:11:49 +0000

    update uspexkit/core.py.
    Signed-off-by: FengGo <gfeng.alan@foxmail.com>

--- uspexkit/core.py
+++ uspexkit/core.py
@@ -229,8 +229,9 @@ def gp(tolerance=0.005,step=1000,n=1,b=1.5,u=0.2,f=1,dat='data',resf='results1')
             file=fd)
     
     density_= mean_prediction[0] # data_[ind[0][im],-1]
-    if ((density_>np.max(y)*1.1 and (density_/density>1.5 or  density/density_>1.5)) or 
-        (density_>np.max(y) and res[imin]>10) ):
+    # if ((density_>np.max(y)*1.1 and (density_/density>1.5 or  density/density_>1.5)) or 
+    #     (density_>np.max(y) and res[imin]>10) ):
+    if res[imin]>10:
        if density_rf[0]/density>1.5 or  density/density_rf[0]>1.5:
           density_ = density*d_scaler
        else:
