25349d639c72c3e823e50ea37d53b4a6f63d4413
fenggo <gfeng.alan@foxmail.com>
2026-07-13 23:03:22 +0800

    write refit file

--- uspexkit/core.py
+++ uspexkit/core.py
@@ -262,6 +262,8 @@ def gp(tolerance=0.005,step=1000,n=1,b=1.5,u=0.03,f=1,dat='data',dft=0,pop=100):
                        volume = atoms_opt.get_volume()
                        density_ = masses / volume / 0.602214129
                        energy = atoms_opt.get_potential_energy()
+                       with open('refit','w') as fr:
+                            print(1,file=fr)
         
     with open('gp.csv','a') as fd:
         # id_ = fd.tell()
