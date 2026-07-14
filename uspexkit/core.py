7e1dfbd431c08cc24f3491ef94575e92bac00137
fenggo <gfeng.alan@foxmail.com>
2026-07-15 07:00:28 +0800

    refit GP

--- uspexkit/core.py
+++ uspexkit/core.py
@@ -265,7 +265,25 @@ def gp(tolerance=0.005,step=1000,n=1,b=1.5,u=0.03,f=1,dat='data',dft=0,pop=100):
                        energy = atoms_opt.get_potential_energy()
                        with open('refit','w') as fr:
                             print(1,file=fr)
-        
+                       subprocess.call("rm gpr_*.pkl", shell=True) 
+                       subprocess.call("rm rfr.pkl", shell=True) 
+                              
+                       # add structure to database
+                       # chdir(data_dir)
+                       with open(f"../{dat}/feature_mlp.csv", "a") as fd:
+                            print(f"0,{feature[0]},{feature[1]},{feature[2]},{feature[3]},"
+                                  f"{feature[4]},{feature[5]},{feature[6]},{feature[7]},{feature[8]},{feature[9]}", file=fd)
+                       with open(f"../{dat}/feature.csv", "a") as fd:
+                            print(f"0,{energy},{feature[1]},{feature[2]},"
+                                  f"{feature[3]},{feature[4]},{feature[5]},{feature[6]},{feature[7]},{feature[8]},{density}",
+                                  file=fd)
+
+                      # atoms_opt.calc = SinglePointCalculator(atoms_opt, energy=energy)
+                      with TrajectoryWriter(f"../{dat}/structures_mlp.traj", mode="a") as traj_w:
+                           traj_w.write(atoms=atoms)
+                      with TrajectoryWriter(f"../{dat}/structures.traj", mode="a") as traj_w:
+                           traj_w.write(atoms=atoms_opt)
+                          
     with open('gp.csv','a') as fd:
         # id_ = fd.tell()
         print(0,',',imin,',',res[imin],',',data_[imin][-1],',',
